bc62c077f1190bb1d6e19acd44eacb06e911ef2f
Quoc-Hwng <phamhung151020@gmail.com>
2025-02-07 15:53:24 +0700

    Update Eslint Rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,12 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 
 export default [
-  { ignores: ['dist'] },
+  {
+    ignores: ['dist', 'vite.config.ts']
+  },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -14,14 +17,15 @@ export default [
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+        sourceType: 'module'
+      }
     },
     settings: { react: { version: '18.3' } },
     plugins: {
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: eslintPluginPrettier
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -29,10 +33,21 @@ export default [
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': [
         'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
+        {
+          arrowParens: 'always',
+          semi: false,
+          trailingComma: 'none',
+          tabWidth: 2,
+          endOfLine: 'auto',
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120,
+          jsxSingleQuote: true
+        }
+      ]
+    }
+  }
 ]
