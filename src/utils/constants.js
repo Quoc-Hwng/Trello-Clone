2cfd5281b83112c8ee3860bfe992eab178cc89e4
Quoc-Hwng <phamhung151020@gmail.com>
2025-02-19 17:46:31 +0700

    change api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,11 @@
-export const API_ROOT = 'http://localhost:4000'
+let apiRoot = 'https://trello-api-brne.onrender.com'
+console.log(import.meta.env)
+// if (process.env.BUILD_MODE === 'dev') {
+//   apiRoot = 'http://localhost:4000'
+// }
+// if (process.env.BUILD_MODE === 'production') {
+
+//   apiRoot = 'https://trello-api-brne.onrender.com/'
+// }
+
+export const API_ROOT = apiRoot
